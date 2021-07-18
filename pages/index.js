20d5e96f087d41c2c1b7ebd76d1ec5875f80aeaa
PoomazEDSS <poomazedss.pp@example.com>
2021-07-18 12:40:37 +0700

    vsesion1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
-
+import React from 'react';
+import { Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
 export default function Home() {
 
   return (
@@ -15,9 +19,35 @@ export default function Home() {
    <Link href="/"><a>Home</a></Link> | 
    <Link href="/about"><a>about</a></Link> | 
    <Link href="/service"><a>service</a></Link> | 
-   <Link href="/products/P002"><a>products</a></Link> | 
+   <Link href="/products/P002"><a>products</a></Link> | </center>
+   <br />
+   <center>
+   <Button variant="outlined" color="primary">
+    Hello 
+   </Button>
+   <br/><br/>
+   <Box component="span" m={1}>
+   <Button variant="contained" color="primary">
+  BOX
+  </Button>
+</Box>
+<br/><br/>
+<ButtonGroup color="primary" aria-label="outlined primary button group">
+  <Button>One</Button>
+  <Button>Two</Button>
+  <Button>Three</Button>
+</ButtonGroup>
+   </center>
+   <center><br/><br/><br/>
+  
    </center>
   </main>  
     </div>
   )
 }
+
+
+
+
+
+
