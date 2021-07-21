4b5c6b3690bd1954de3c90227662a525e4708212
PoomazEDSS <poomazedss.pp@example.com>
2021-07-21 15:54:05 +0700

    Adminlteversion1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+
+
+// module.exports = {
+//   reactStrictMode: true,
+// }
+// const withSass = require("@zeit/next-sass");
+// 
+const withSass = require("@zeit/next-sass");
+
 module.exports = {
   reactStrictMode: true,
 }
+
